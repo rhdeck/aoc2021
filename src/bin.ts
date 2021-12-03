16502e397b145ed4e945beb140c057563b976c49
Ray Deck <ray@raydeck.com>
2021-12-03 07:12:27 -0500

    fix: don't run bin when isDocumenting is true

--- src/bin.ts
+++ src/bin.ts
@@ -3,8 +3,9 @@ import commander from "commander";
 import { execSync } from "child_process";
 commander.description("Run advent of code answers");
 commander.arguments("<day-of-advent>");
-commander.parse(process.argv);
-const command = `npx ts-node src/${commander.args[0]}.ts`;
-execSync(command, { stdio: "inherit" });
-
+if (!commander.isDocumenting) {
+  commander.parse(process.argv);
+  const command = `npx ts-node src/${commander.args[0]}.ts`;
+  execSync(command, { stdio: "inherit" });
+}
 export { commander };
