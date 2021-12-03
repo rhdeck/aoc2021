2390b42f0f035b8cc73881cb11c762bff34841ef
Ray Deck <ray@raydeck.com>
2021-12-03 06:58:57 -0500

    Day 1 (#2)
    * feat: on the first day of advent
    
    * feat: update bin and docs for easy run

--- src/bin.ts
+++ src/bin.ts
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 import commander from "commander";
+import { execSync } from "child_process";
+commander.description("Run advent of code answers");
+commander.arguments("<day-of-advent>");
 commander.parse(process.argv);
+const command = `npx ts-node src/${commander.args[0]}.ts`;
+execSync(command, { stdio: "inherit" });
+
 export { commander };
